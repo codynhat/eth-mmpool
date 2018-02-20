db49d27fd015e27775e0933f3a848a40a9119786
Cody Hatfield <cody.hatfield@me.com>
2018-02-19 17:20:05 -0800

    Event submission and reward submission

--- contracts/EntrySubmission.sol
+++ contracts/EntrySubmission.sol
@@ -6,8 +6,11 @@ contract EntrySubmission {
   struct Entry {
     uint entryHash;
     uint bracketCount;
+    uint reward;
     bool exists;
   }
+
+  event EntrySubmitted(address entrant);
 
   mapping (address => Entry) public entries;
 
@@ -21,6 +24,20 @@ contract EntrySubmission {
     // Valid submission time. March 15, 2018 8:00AM PST
     require(now < 1521126000);
 
-    entries[msg.sender] = Entry(entryHash, bracketCount, true);
+    entries[msg.sender] = Entry(entryHash, bracketCount, 0, true);
+    EntrySubmitted(msg.sender);
+  }
+
+  function submitRewards(address[] entrants, uint[] rewards) public {
+    // Predetermined multi-sig wallet
+    require(msg.sender == 0x627306090abaB3A6e1400e9345bC60c78a8BEf57);
+
+    require(entrants.length == rewards.length);
+
+    for (uint i = 0; i < entrants.length; i++) {
+      address entrant = entrants[i];
+      uint reward = rewards[i];
+      entries[entrant].reward = reward;
+    }
   }
 }
--- test/entry_submission.js
+++ test/entry_submission.js
@@ -7,7 +7,8 @@ contract('EntrySubmission', accounts => {
     let account = accounts[0]
     let startingBalance = web3.eth.getBalance(EntrySubmission.address)
     return EntrySubmission.deployed().then(instance => {
-      return instance.submitEntry(22, 10, {from: account, value: fee}).then(() => {
+      return instance.submitEntry(22, 10, {from: account, value: fee}).then(result => {
+        assert.equal(result.logs[0].args.entrant, account, "Account does not match event")
         return instance.entries.call(account)
       })
     }).then(entry => {
@@ -23,7 +24,8 @@ contract('EntrySubmission', accounts => {
     let account = accounts[1]
     let startingBalance = web3.eth.getBalance(EntrySubmission.address)
     return EntrySubmission.deployed().then(instance => {
-      return instance.submitEntry(22, 10, {from: account, value: fee}).then(() => {
+      return instance.submitEntry(22, 10, {from: account, value: fee}).then(result => {
+        assert.equal(result.logs[0].args.entrant, account, "Account does not match event")
         return instance.entries.call(account)
       })
     }).then(entry => {
@@ -60,6 +62,38 @@ contract('EntrySubmission', accounts => {
     }).then(() => {
       let newBalance = web3.eth.getBalance(EntrySubmission.address)
       assert.deepEqual(startingBalance.plus(fee), newBalance)
+    })
+  })
+
+  it("should submit rewards", () => {
+    let account = accounts[0]
+    return EntrySubmission.deployed().then(instance => {
+      return instance.submitRewards(accounts, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1], {from: account})
+        .then(result => instance.entries.call(account))
+        .then(entry => {
+          assert.equal(entry[2].toNumber(), 10, "reward not saved")
+          return instance.entries.call(accounts[8])
+        })
+        .then(entry => {
+          assert.equal(entry[2].toNumber(), 2, "reward not saved")
+        })
+    })
+  })
+
+  it("should not submit rewards on bad sender", () => {
+    let account = accounts[1]
+    return EntrySubmission.deployed().then(instance => {
+      return instance.submitRewards(accounts, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], {from: accounts[0]})
+        .then(() => instance.submitRewards(accounts, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1], {from: account}))
+        .catch(err => {
+          assert.isNotNull(err, "Expected error but none was found")
+        })
+        .then(() => {
+          return instance.entries.call(account)
+            .then(entry => {
+              assert.equal(entry[2].toNumber(), 0, "reward not equal to 0")
+            })
+        })
     })
   })
 
